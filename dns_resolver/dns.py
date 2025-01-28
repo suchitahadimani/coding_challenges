31e929426fa76aab3488b4adfa745b6da74491c3
Suchita Hadimani <suchitahadimani@mac.mynetworksettings.com>
2025-01-28 17:42:18 -0500

    dns part1

--- dns_resolver/dns.py
+++ dns_resolver/dns.py
@@ -1,22 +1,9 @@
 import struct
+import socket
 
+# PART ONE SETTING UP DNS
 
-#packing data
-
-#the > means its big endian
-format_string = '>I f 5s' 
-binary_data = struct.pack(format_string, 42, 3.14, b'hello')
-print(binary_data)  # Output: b'*\x00\x00\x00\xc3\xf5H@\x68\x65\x6c\x6c\x6f'
-
-
-# unpacking the binary data
-original_data = struct.unpack(format_string, binary_data)
-print(original_data)  # Output: (42, 3.14, b'hello')
-
-
-
-
-#dns header, > for big endian and H is an unsigned short aka two bytes or 16 bits which is the lenght of each part 
+#dns header, > for big endian and H is an unsigned short aka two bytes or 16 bits which is the lenght of each part (eg qdcount is 2 bytes)
 dns_header_format = ">HHHHHH"
 
 #random? shouldn't matter? 
@@ -37,3 +24,51 @@ dns_header = struct.pack(
     nscount,
     arcount
 )
+
+#ok applying similiar logic to the dns_question field, the only thing is that the url can be variable-length
+
+#qname will need a special function
+
+def convert_qname(qname):
+    parts = qname.split(".")
+    return b"".join(len(part).to_bytes(1, "big") + part.encode() for part in parts) + b"\x00"
+
+qname = convert_qname("www.google.com")
+qtype = 1
+qclass = 1
+
+#s is for char[] or strings essentially
+dns_question_format = f">{len(qname)}sHH"
+dns_question = struct.pack(
+    dns_question_format,
+    qname,
+    qtype,
+    qclass
+)
+
+
+dns_query = dns_header + dns_question
+print("query: ", dns_query)
+
+
+
+# PART TWO ACTUALLY SENDING IT USING SOCKETS
+#now that the dns is in shape, all thats needed is sending it across a socket to a server
+
+HOST = '8.8.8.8'   #googles port and ip
+PORT = 53  
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+client_socket.sendall(dns_query)
+
+response = client_socket.recv(1024)
+client_socket.close()
+
+print(f"Received from server: {response}")
+
+# PART THREE EXTRACT THE RESPONSE AND DETERMINE THE IP RESPONSE
+
+
+
+#PART FOUR -- GENERALIZE, PUT EVERYTHING INTO METHODS, MAKE IT REUSABLE
