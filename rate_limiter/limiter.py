43f39736edfc824ce1c5a94328d1b294fc6f9827
Suchita Hadimani <suchita.careers@gmail.com>
2025-03-04 23:42:03 -0500

    set up venv

--- rate_limiter/limiter.py
+++ rate_limiter/limiter.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+import time
 
 app = Flask(__name__)
 
-#you create apis in flask by using app routers.
-@app.route('/limited', methods = ["GET"])
-def limited():
-    return "limited"
+bucket_size = 10
+
+def add_token():
+    global bucket_size
+    time.sleep(1)
+    bucket_size += 1
 
 #you create apis in flask by using app routers.
 @app.route('/limited', methods = ["GET"])
 def limited():
-    return "limited"
+    global bucket_size
+    
+    return jsonify({"message": "Limited"})
+
+#you create apis in flask by using app routers.
+@app.route('/unlimited', methods = ["GET"])
+def unlimited():
+    return jsonify({"message": "Unlimited!"})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)  # Change 8080 to your desired port
